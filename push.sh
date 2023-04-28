fce0e88f0773b5b1ea09a7a3c974615ea63cdd20
slowisfast <slowisfast2030@163.com>
2023-04-28 13:15:40 +0800

    slow is fast

--- push.sh
+++ push.sh
@@ -6,3 +6,4 @@ git push
 # 添加远程库的时候注意地址
 # git remote add origin git@github.com:slowisfast2030/metacoin-box.git
 # git remote add origin https://github.com/slowisfast2030/metacoin-box.git
+# 因为配置了ssh，所以用第一种方式，如果没有配置ssh，用第二种方式
