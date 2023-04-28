f2843f9b849ae269ff764b65103185059cc7aaf3
slowisfast <slowisfast2030@163.com>
2023-04-28 13:14:52 +0800

    slow is fast

--- push.sh
+++ push.sh
@@ -1,2 +1,8 @@
+git pull
 git add *
 git commit -m "slow is fast"
+git push
+
+# 添加远程库的时候注意地址
+# git remote add origin git@github.com:slowisfast2030/metacoin-box.git
+# git remote add origin https://github.com/slowisfast2030/metacoin-box.git
